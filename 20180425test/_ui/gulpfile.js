9146860a4737a95ab13138c891b8b9d116d0d0d6
黄卉 <326028030@qq.com>
2018-04-26 17:32:20 +0800

    添加gulp配置test

---
+++ 20180425test/_ui/gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp'),
+   renameRev = require('gulp-rename-rev');
+
+
+//添加版本号，并将对应的文件写到json文件中。
+gulp.task('rev', () =>
+    gulp.src(['src/**/*.js','src/**/*.less','src/**/*.css'])
+        .pipe(renameRev())
+        .pipe(gulp.dest('assets'))
+        .pipe(renameRev.manifest())
+        .pipe(gulp.dest('assets'))
+);
+
+
+
